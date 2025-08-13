ea85f292f617b059b9ab2c8428b65fa568f9c7b7
Gedeon Ineza ndayambaje <ndagedeo061@gmail.com>
2025-08-13 11:35:39 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const menu = document.getElementById('menu');
+  const navLinks = document.getElementById('navLinks');
+
+  menu.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+  });
