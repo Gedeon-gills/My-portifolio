3d2e346ce6c9515d1b02d49aa34949073eaae557
gedeon_gills <ndagedeo@gmail.com>
2025-11-28 10:08:36 +0200

    updated html,css and js

--- script.js
+++ script.js
@@ -25,3 +25,20 @@ themeToggler.addEventListener('click', () => {
     themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
 
 });
+
+const backToTop = document.getElementById("backToTop");
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 300) {
+            backToTop.classList.add("show");
+        } else {
+            backToTop.classList.remove("show");
+        }
+    });
+
+    backToTop.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
